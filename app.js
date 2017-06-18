948e0a9030fa86ddf5097390007a7bc1c9d5f129
Luis Quinones <alexx813@gmail.com>
2017-06-17 22:25:25 -0500

    comments added

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require("express"),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
     Campground = require("./models/campground"),
+    Comment = require("./models/comment"),
     seedDB = require("./seeds")
+
 
 seedDB();
 mongoose.connect('mongodb://localhost/yelpcamp');
@@ -23,7 +25,7 @@ app.get("/campgrounds", function(req, res) {
         if (err) {
             console.log(err);
         } else {
-            res.render("index", { campgrounds: allCampgrounds });
+            res.render("campgrounds/index", { campgrounds: allCampgrounds });
         }
     });
 });
@@ -48,22 +50,68 @@ app.post("/campgrounds", function(req, res) {
 
 //NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res) {
-    res.render("new.ejs");
+    res.render("campgrounds/new");
 });
 
 // SHOW - shows more info about one campground
 app.get("/campgrounds/:id", function(req, res) {
-    //find the campground with provided ID
-    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
+        //find the campground with provided ID
+        Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(foundCampground)
+                    //render show template with that campground
+                res.render("campgrounds/show", { campground: foundCampground });
+            }
+        });
+    })
+    //===============================
+    //  COMMENTS ROUTES
+    //===============================
+
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+    // find campground by id
+    Campground.findById(req.params.id, function(err, campground) {
         if (err) {
             console.log(err);
         } else {
-            console.log(foundCampground)
-                //render show template with that campground
-            res.render("show", { campground: foundCampground });
+            res.render("comments/new", { campground: campground });
+        }
+    })
+});
+
+app.post("/campgrounds/:id/comments", function(req, res) {
+    //lookup campground using ID
+    Campground.findById(req.params.id, function(err, campground) {
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, function(err, comment) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect('/campgrounds/' + campground._id);
+                }
+            });
         }
     });
-})
+    //create new comment
+    //connect new comment to campground
+    //redirect campground show page
+});
+//create new comment
+//connect new comment to campground
+//redirect campground show page
+
+
+
+
+
+
 
 app.listen(3000, function(req, res) {
     console.log("SERVER STARTED");
