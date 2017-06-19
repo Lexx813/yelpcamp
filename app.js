94f0afa51b654b695e86c86a3eff3cf32c17341d
Luis Quinones <alexx813@gmail.com>
2017-06-19 08:59:39 -0500

    styled comments

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ seedDB();
 mongoose.connect('mongodb://localhost/yelpcamp');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 seedDB();
 let db = mongoose.connection;
 
